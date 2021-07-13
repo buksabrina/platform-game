506b17817da2282bca58ad6a02719a469cc168da
buksabrina <buksabrina@gmail.com>
2021-07-13 14:21:36 -0700

    level 1

--- source/player.gd
+++ source/player.gd
@@ -6,6 +6,8 @@ var velocity := Vector2.ZERO
 export var walk_speed := 100
 export var jump_speed := 400
 
+export var traction := 0.05
+
 export var gravity := 2000
 
 export var coins := 0
@@ -35,7 +37,9 @@ func _physics_process(delta):
 		x += walk_speed
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		velocity.y -=jump_speed
-	velocity.x=x
+	#velocity.x=x
+	velocity.x=lerp(velocity.x,x,traction)
+	velocity.x += (x-velocity.x)*traction
 	velocity.y+= gravity*delta
 	#position+=velocity*delta
 	velocity = move_and_slide(velocity,Vector2.UP)
