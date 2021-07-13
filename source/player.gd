33282a3f27290a403956abd8c5a68cac91cb0ec2
buksabrina <buksabrina@gmail.com>
2021-07-13 13:32:25 -0700

    coins working

--- source/player.gd
+++ source/player.gd
@@ -4,21 +4,28 @@ extends KinematicBody2D
 
 var velocity := Vector2.ZERO
 export var walk_speed := 100
+export var jump_speed := 400
 
 export var gravity := 2000
 
+export var coins := 0
+
 func change_animation():
-	if velocity.x==0:
-		$AnimatedSprite.play("idle")
-	else: 
-		$AnimatedSprite.play ("walk")
-		if velocity.x < 0 :
-			$AnimatedSprite.flip_h=true
-		elif velocity.x > 0:
-			$AnimatedSprite.flip_h=false
+	if is_on_floor():
+		
+		if velocity.x==0:
+			$AnimatedSprite.play("idle")
+		else: 
+			$AnimatedSprite.play ("walk")
+			if velocity.x < 0 :
+				$AnimatedSprite.flip_h=true
+			elif velocity.x > 0:
+				$AnimatedSprite.flip_h=false
+	else:
+		$AnimatedSprite.play("jump")
 
 func _ready():
-	pass
+	$Camera2D.smoothing_enabled=true
 
 func _physics_process(delta):
 	var x = 0
@@ -26,6 +33,8 @@ func _physics_process(delta):
 		x -= walk_speed
 	if Input.is_action_pressed("walk_right"):
 		x += walk_speed
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		velocity.y -=jump_speed
 	velocity.x=x
 	velocity.y+= gravity*delta
 	#position+=velocity*delta
@@ -33,3 +42,7 @@ func _physics_process(delta):
 
 func _process(delta):
 	change_animation()
+
+func collect_coin():
+	coins += 1
+	$HUD.set_coins(coins)
