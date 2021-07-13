d079093d9720632cbfd1e122c13aa938a5c65636
buksabrina <buksabrina@gmail.com>
2021-07-13 11:18:18 -0700

    added coin

--- source/player.gd
+++ source/player.gd
@@ -5,6 +5,8 @@ extends KinematicBody2D
 var velocity := Vector2.ZERO
 export var walk_speed := 100
 
+export var gravity := 2000
+
 func change_animation():
 	if velocity.x==0:
 		$AnimatedSprite.play("idle")
@@ -25,8 +27,9 @@ func _physics_process(delta):
 	if Input.is_action_pressed("walk_right"):
 		x += walk_speed
 	velocity.x=x
-	position.x+=velocity.x*delta
-	
+	velocity.y+= gravity*delta
+	#position+=velocity*delta
+	velocity = move_and_slide(velocity,Vector2.UP)
 
 func _process(delta):
 	change_animation()
